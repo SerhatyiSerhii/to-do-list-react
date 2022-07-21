37aa5f15ba733d1f7fa49954c5d8e51855637cdb
Serhii Serhatyi <serhatyi.serhii@gmail.com>
2022-07-21 21:11:52 +0300

    updates server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-import express from 'express';
-
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const errorHandler = require('./middleware/errorHandler');
 const port = 8080;
 const host = 'localhost';
 
-const app = express();
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+app.use(cors());
+
+app.use(express.urlencoded({extended: false}));
 
 app.use(express.json());
 
@@ -12,79 +21,12 @@ app.use((_req, res, next) => {
     next();
 });
 
-let users = [
-    {
-        name: 'testUser',
-        password: '123456789',
-        id: 1,
-    }
-];
+// routes
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/users', require('./routes/api/users'));
 
-app.get('/users/:id', (req, res) => {
-    const userIndex = findUserIndex(req, res);
-
-    const user = users[userIndex];
-
-    res.json(user);
-});
-
-app.post('/users', (req, res) => {
-    const user = req.body.user;
-
-    if (!isUserVaild(user)) {
-        res.status(400).json({errorMessage: 'Some required fields are missed!'});
-    }
-
-    user.id = Date.now();
-
-    users.push(user);
-
-    res.json(user);
-})
-
-app.put('/users/:id', (req, res) => {
-    const userIndex = findUserIndex(req, res);
-
-    const user = req.body.user;
-
-    if(!isUserVaild(user)) {
-        res.status(400).json({errorMessage: 'Some required fields are missed!'});
-    }
-
-    user.id = +req.params.id;
-
-    users[userIndex] = req.body.user;
-
-    res.json(user);
-});
-
-app.delete('/users/:id', (req, res) => {
-    const userId = +req.params.id;
-
-    if (!users.some(user => user.id === userId)) {
-        res.status(400).json({errorMessage: 'User does not exist!'});
-        return;
-    }
-
-    users = users.filter(user => user.id !== userId);
-
-    res.json({});
-});
-
-const isUserVaild = (user) => Boolean(user.name) && Boolean(user.password);
-
-const findUserIndex = (req, res) => {
-    const index = users.findIndex((user) => {
-        return user.id === +req.params.id;
-    });
-
-    if (index === -1) {
-        res.status(400).json({errorMessage: 'User does not exist!'});
-        return;
-    }
-
-    return index;
-}
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running at http://${host}:${port}`);

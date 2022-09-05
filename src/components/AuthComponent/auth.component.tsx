4d5fd745069567fdd8d98093e293b1bb5bb11fbf
Serhii Serhatyi <serhatyi.serhii@gmail.com>
2022-09-05 09:04:08 +0300

    added register functionality

--- src/components/AuthComponent/auth.component.tsx
+++ src/components/AuthComponent/auth.component.tsx
@@ -1,35 +1,51 @@
 
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ButtonComponent from "../ButtonComponent/button.component";
 import InputsComponent from "../InputsComponent/inputs.component";
 import axios from "../../api/axios";
-import AuthContext from "../../context/AuthProvider";
+import useAuth from "../../hooks/useAuth";
 
 const AUTH_URL = '/auth';
+const REGISTER_URL = '/register';
 
 export function AuthComponent() {
-    const { setAuth }  = React.useContext(AuthContext) as any;
+    const nameRef = useRef();
+    const pwdRef = useRef();
+    const checkPwdRef = useRef();
+
+    const { setAuth } = useAuth();
     const [nameErrors, setNameErrors] = useState<boolean | undefined>(true);
     const [passwordErrors, setPasswordErrors] = useState<boolean | undefined>(true);
+    const [confirmPwd, setConfirmPwdErrors] = useState<boolean | undefined>(true);
     const [invalid, setInvalid] = useState<boolean>(true);
     const [name, setName] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const [accessToken, setAccessToken] = useState<string>('');
+    const [checkPwd, setCheckPwd] = useState<string>('');
 
     let navigate = useNavigate();
 
     useEffect(() => {
-        setInvalid((!nameErrors && !passwordErrors) ? false : true);
-    }, [nameErrors, passwordErrors]);
+        if (document.querySelector('.confirm-password')) {
+            setInvalid((!nameErrors && !passwordErrors && !confirmPwd) ? false : true);
+        } else {
+            setInvalid((!nameErrors && !passwordErrors) ? false : true);
+        }
+    }, [nameErrors, passwordErrors, confirmPwd]);
+
+    useEffect(() => {
+        setConfirmPwdErrors(password === checkPwd ? false : true);
+    }, [password, checkPwd]);
 
     const handleInput = (event: ChangeEvent<HTMLInputElement>): void => {
         if (event.target.className === 'name-input') {
             setNameErrors(event.target.value.trim() ? false : true);
             setName(event.target.value.trim());
-        } else {
+        } else if (event.target.className === 'password-input') {
             setPasswordErrors(event.target.value.trim() ? false : true);
             setPassword(event.target.value.trim());
+        } else {
+            setCheckPwd(event.target.value.trim());
         }
     }
 
@@ -43,14 +59,14 @@ export function AuthComponent() {
                     password
                 }),
                 {
-                    headers: {'Content-Type': 'application/json',},
+                    headers: { 'Content-Type': 'application/json', },
                     withCredentials: true
                 }
             );
             const accessToken = result.data.accessToken;
             const toDoList = result.data.toDoList;
             const userId = result.data.userId;
-            setAuth({name, password, accessToken, toDoList, userId});
+            setAuth({ name, password, accessToken, toDoList, userId });
             navigate('/list', { replace: true });
         } catch (err) {
             console.error(err);
@@ -59,23 +75,65 @@ export function AuthComponent() {
 
     const handleSignUp = (event?: React.MouseEvent<HTMLButtonElement, MouseEvent> | undefined): void => {
         event?.preventDefault();
+
+        setName('');
+        setNameErrors(true);
+        setPassword('');
+        setPasswordErrors(true);
+
+        (nameRef.current as any).value = '';
+        (pwdRef.current as any).value = '';
+
         navigate('/sign-up', { replace: true });
     }
 
     const handleBackClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | undefined): void => {
         event?.preventDefault();
+
+        setName('');
+        setNameErrors(true);
+        setPassword('');
+        setPasswordErrors(true);
+        setCheckPwd('');
+        setConfirmPwdErrors(true);
+
+        (nameRef.current as any).value = '';
+        (pwdRef.current as any).value = '';
+        (checkPwdRef.current as any).value = '';
+
         navigate('/', { replace: true });
     }
 
-    const handleNewUser = (event?: React.MouseEvent<HTMLButtonElement, MouseEvent> | undefined): void => {
+    const handleNewUser = async (event?: React.MouseEvent<HTMLButtonElement, MouseEvent> | undefined): Promise<void> => {
         event?.preventDefault();
-        navigate('/list', { replace: true });
+
+        try {
+            const result = await axios.post(REGISTER_URL,
+                JSON.stringify({
+                    name,
+                    password
+                }),
+                {
+                    headers: { 'Content-Type': 'application/json', },
+                    withCredentials: true
+                }
+            );
+
+            const accessToken = result.data.accessToken;
+            const toDoList = result.data.toDoList;
+            const userId = result.data.userId;
+            setAuth({ name, password, accessToken, toDoList, userId });
+
+            navigate('/list', { replace: true });
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return (
         <div className="auth-container">
             <form onSubmit={() => handleSignIn()}>
-                <InputsComponent handleChange={handleInput} />
+                <InputsComponent handleChange={handleInput} nameRef={nameRef} pwdRef={pwdRef} checkPwdRef={checkPwdRef} />
                 <div className="auth-form-btns">
                     {
                         window.location.pathname === '/' ?
